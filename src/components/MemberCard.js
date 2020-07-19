d9a3aa1e2921367028e487849b39734626f0dc13
Valeria Vassallo <valevassallo1@gmail.com>
2020-07-18 20:39:53 -0500

    Add profile pictures

--- src/components/MemberCard.js
+++ src/components/MemberCard.js
@@ -4,7 +4,7 @@ function MemberCard({ member, colMdSize }) {
   return(
     <div className={`col-md-${colMdSize} col-sm-6`}>
 			<div className="card">
-				<img className="card-img-top" src="https://www.clker.com/cliparts/L/C/Q/c/G/1/head-of-person-md.png" alt="team member" />
+				<img className="card-img-top" src={member.pic} alt="team member" />
 				<div className="card-body">
 					<h4 className="card-title">{member.name}</h4>
 					<p className="card-text">{member.role}</p>
